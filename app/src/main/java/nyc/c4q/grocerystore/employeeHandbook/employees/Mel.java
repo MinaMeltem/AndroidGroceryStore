2e7e1852feaba9563302695ac57c35093dd3f48e
MinaMeltem <meltem@gmail.com>
2016-10-24 11:39:17 -0400

    add profile Employee Handbook

---
+++ app/src/main/java/nyc/c4q/grocerystore/employeeHandbook/employees/Mel.java
@@ -0,0 +1,26 @@
+package nyc.c4q.grocerystore.employeeHandbook.employees;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import nyc.c4q.grocerystore.R;
+
+/**
+ * Created by meltemyildirim on 10/23/16.
+ */
+
+public class Mel extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.meltem_yildirim,container,false);
+    }
+
+
+}
